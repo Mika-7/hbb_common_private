7b6941b876f7fb7a360694a34aee00496f720c0f
Mika-7 <3125986455@qq.com>
2025-09-07 10:48:22 +0800

    隐藏网络设置

--- src/config.rs
+++ src/config.rs
@@ -77,7 +77,7 @@ lazy_static::lazy_static! {
     // pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("hide-network-settings".to_string(), "true".to_string());
+        map.insert("hide-network-settings".to_string(), "Y".to_string());
         RwLock::new(map)
     };
 
